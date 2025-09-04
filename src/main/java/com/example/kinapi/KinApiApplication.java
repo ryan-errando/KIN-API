ba846a95d5b6a73b34ba48db3f1a016b048d588a
ryan-errando <errandoryan@gmail.com>
2025-09-04 10:36:19 +0700

    Initial commit

---
+++ src/main/java/com/example/kinapi/KinApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinApiApplication.class, args);
+    }
+
+}
