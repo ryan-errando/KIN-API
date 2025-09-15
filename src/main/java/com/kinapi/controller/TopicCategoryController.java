85970a499a727ce9ee6ac67003d576ad3fb17395
ryan-errando <errandoryan@gmail.com>
2025-09-15 22:03:53 +0700

    chore: delete unused import

--- src/main/java/com/kinapi/controller/TopicCategoryController.java
+++ src/main/java/com/kinapi/controller/TopicCategoryController.java
@@ -4,7 +4,6 @@ import com.kinapi.common.entity.BaseResponse;
 import com.kinapi.service.TopicCategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
