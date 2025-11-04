85aba43d8684b666a5421e408c8575949bf1c051
ryan-errando <errandoryan@gmail.com>
2025-11-04 09:46:20 +0700

    feat: add new entity to store family daily question and question response

--- src/main/java/com/kinapi/common/entity/FamilyDailyQuestion.java
+++ src/main/java/com/kinapi/common/entity/FamilyDailyQuestion.java
@@ -15,13 +15,13 @@ import java.util.UUID;
 @Getter
 @Setter
 @Entity
-@Table(name = "family_members")
+@Table(name = "family_daily_question")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @DynamicInsert
 @DynamicUpdate
-public class FamilyMembers implements Serializable {
+public class FamilyDailyQuestion implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
@@ -29,24 +29,37 @@ public class FamilyMembers implements Serializable {
     @UuidGenerator(style = UuidGenerator.Style.TIME)
     private UUID id;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private Users user;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id", nullable = false)
     private FamilyGroups group;
 
-    @Column(name = "role")
-    private String role;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "question_id", nullable = false)
+//    private List<Questions> questions;
 
-    @Column(name = "joined_time")
+    @Column(name = "total_members")
+    private Integer totalMembers;
+
+    @Column(name = "answered_count")
+    private Integer answeredCount;
+
+    @Column(name = "assigned_date")
     @CreationTimestamp
-    private LocalDateTime joinedTime;
+    private LocalDateTime assignedDate;
 
-    @OneToMany(mappedBy = "createdBy", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
-    private List<CalendarEvents> calendarEvents;
+    @Column(name = "is_completed")
+    @Builder.Default
+    private Boolean isCompleted = false;
 
-    @OneToMany(mappedBy = "familyMembers", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    @Column(name = "completed_at")
+    private LocalDateTime completedAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "familyDailyQuestion", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<DailyQuestionResponse> dailyQuestionResponses;
 }
+
+
